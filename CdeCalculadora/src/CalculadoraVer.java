58b96070aa85b22ee173c82805d3b7d1564afab4
mario <marioiram2000@gmail.com>
2019-04-16 10:39:49 +0200

    Añadida la clase

---
+++ CdeCalculadora/src/CalculadoraVer.java
@@ -0,0 +1,27 @@
+
+public class CalculadoraVer {
+
+	private int n1, n2;
+
+	public CalculadoraVer(int n1, int n2) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+
+	private int suma(int num1, int num2) {
+		return num1+num2;
+	}
+	
+	private int resta(int num1, int num2) {
+		return num1-num2;
+	}
+	
+	private int multiplicacion(int num1, int num2) {
+		return num1*num2;
+	}
+	
+	private int division(int num1, int num2) {
+		return num1*num2;
+	}
+}
